05bf0b94abff25949bd0dc552d912ca89668d796
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-04-19 20:50:12 +0100

    Initialise gravatar

--- main.py
+++ main.py
@@ -20,6 +20,18 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+
+# Initialise gravatar
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
 # Login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
